fc1e845e3f97773bb1f2172f1301f69d85f19eef
Andrie Prasetyo <andriepu@gmail.com>
2021-06-08 08:56:21 +0700

    Handle pdf

--- api/routers/confluence.groomings.show._id/_get-issues-detail.js
+++ api/routers/confluence.groomings.show._id/_get-issues-detail.js
@@ -2,4 +2,6 @@ const axios = require('~/api/modules/axios/--internal');
 
 module.exports = keys => axios.get('/jira/search', {
   params: { keys: keys.join(',') },
-}).then(({ data }) => data.data);
+}).then(({ data }) => (
+  data.data
+));
